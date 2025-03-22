7d90a54cda0828c4661cfde99d1d61d336e8acb8
Illia Stasiuk <www.illiastas@gmail.com>
2025-03-22 16:07:22 +0200

    Project generated

---
+++ src/main/java/ua/edu/ukma/stasiuk/nativeplayground/NativePlaygroundApplication.java
@@ -0,0 +1,13 @@
+package ua.edu.ukma.stasiuk.nativeplayground;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NativePlaygroundApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NativePlaygroundApplication.class, args);
+    }
+
+}
